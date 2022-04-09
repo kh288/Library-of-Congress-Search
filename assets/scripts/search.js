4e91d62739cd52a748ab70c36d71029a3f25f224
rich p <richardferry3@gmail.com>
2022-04-08 22:13:17 -0400

    add function to append elements with information

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -13,7 +13,7 @@ function getLibraryAPI(inputLink) {
 }
 
 getLibraryAPI(queryURL);
-
+// load out information to page, titles, dates, subjects,descriptions, and links
 function loadResults(results) {
     for(var i = 0; i < results.length; i++){
         var cardEl = $("<div>");
@@ -22,7 +22,25 @@ function loadResults(results) {
         cardTitleEl.text(results[i].title);
         cardTitleEl.appendTo(cardEl);
         
+        var cardDateEl = $("<p>");
+        cardDateEl.text(results[i].date);
+        cardDateEl.appendTo(cardEl);
+
+        var cardSubjectEl = $("<p>");
+        cardSubjectEl.text(results[i].subject.join(', '));
+        cardSubjectEl.appendTo(cardEl);
+
+        var cardDescriptionEl = $("<p>");
+        cardDescriptionEl.text(results[i].description[0]);
+        cardDescriptionEl.appendTo(cardEl);
+
+        var cardUrlEl = $("<a>");
+        cardUrlEl.text('Read more');
+        cardUrlEl.attr('href',results[i].url)
+        cardUrlEl.appendTo(cardEl);
+
         cardEl.appendTo(searchContentEl);
+        
 
         console.log(results[i].title);
         console.log(results[i].date);
