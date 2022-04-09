5f31fc7d3fcfe7625048554fc3c125a105c5b615
John Netzel <johnrnetzel@gmail.com>
2022-04-08 17:43:29 -0800

    Parse through results returned from API query

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -1,11 +1,25 @@
-
+var queryURL = window.sessionStorage.getItem("inputURL");
+queryURL = JSON.parse(queryURL);
 
 function getLibraryAPI(inputLink) {
     fetch(inputLink)
     .then(function(response) {
         return response.json();
     }).then(function(data) {
-        //console.log(data);
+        loadResults(data.results);
     })
 }
-// getLibraryAPI(link);
+
+getLibraryAPI(queryURL);
+
+function loadResults(results) {
+    for(var i = 0; i < results.length; i++){
+        console.log(results[i].title);
+        console.log(results[i].date);
+        console.log(results[i].subject); // An array
+        console.log(results[i].description); // Also an array
+        console.log(results[i].url);
+        console.log("-------------------------------------------");
+    }
+}
+console.log(queryURL);
